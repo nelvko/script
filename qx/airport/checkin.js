67a6ad9d01c9e379d3b04c7522fdc5f1c4b4fa05
libm <nelvko@qq.com>
2024-11-22 16:00:51 +0800

    fix

--- qx/airport/checkin.js
+++ qx/airport/checkin.js
@@ -1,4 +1,4 @@
-const headers = JSON.parse($prefs.valueForKey("clash_headers"))
+const headers = JSON.parse($prefs.valueForKey("airport_headers"))
 
 const url = "https://cylink.biz/user/checkin";
 const method = "POST";
@@ -13,13 +13,15 @@ $task.fetch(myRequest).then(response => {
   const res = JSON.parse(decodeURIComponent(response.body))
   if (res.ret === 0) {
     $notify("机场签到", "✅成功", res.msg); // Success!
-
   } else {
-    $notify("机场签到", "❌失败", res.msg); // Error!
+    $notify("机场签到", "❌失败,请重新获取Cookie", res.msg); // Error!
+    $prefs.removeValueForKey("airport_headers")
+
   }
   $done();
 }, reason => {
   // reason.error
-  $notify("签到请求", "❌失败", reason.error); // Error!
+  $notify("签到请求", "❌失败，请重新获取Cookie", reason.error); // Error!
+  $prefs.removeValueForKey("airport_headers")
   $done();
 });
--- qx/airport/get_cookie.js
+++ qx/airport/get_cookie.js
@@ -1,12 +1,8 @@
-let today = new Date().getDate()
-const flag = JSON.parse($prefs.valueForKey("clash_headers_flag") || -1)
-if (today === flag) {
+if ($prefs.valueForKey("airport_headers")) {
   $done({})
 }
 const headers = $request.headers
-$prefs.setValueForKey(today, "clash_headers_flag")
-
-let ok = $prefs.setValueForKey(JSON.stringify(headers), "clash_headers")
+let ok = $prefs.setValueForKey(JSON.stringify(headers), "airport_headers")
 if (ok) {
     $notify("机场Cookie", "✅获取成功", "")
     console.log("success :get cookie ");
